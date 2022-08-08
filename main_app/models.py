bd6861ecd032ef8a7214cc0c69088d309e27ffc2
lilliesheely <lilliesheely@gmail.com>
2022-08-08 11:59:49 -0700

    Details Page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
+from inspect import walktree
+from xml.parsers.expat import model
 from django.db import models
 
+
 # Create your models here.
+class Plant(models.Model): 
+    laymen_name = models.CharField(max_length=100)
+    latin_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=20)
+    light = models.CharField(max_length=50)
+    water = models.CharField(max_length=200)
+    maintenance_level = models.IntegerField()
+
+ 
--- main_app/views.py
+++ main_app/views.py
@@ -1,37 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Plant
 # Create your views here.
 
 
-class Plant: 
-    def __init__(self, laymen_name, latin_name, location, light, water, maintenance_level):
-        self.laymen_name = laymen_name
-        self.latin_name = latin_name
-        self.location = location
-        self.light = light
-        self.water = water
-        self.maintenance_level = maintenance_level
-
-plants = [
-    Plant('Guiana Chestnut(Money Tree)', 'Pachira aquatica', 'indoor', 'indirect light', '1-2 per week', 1 ),
-    Plant('Peace Lily', 'Pachira aquatica', 'indoor', 'bright, indirect', 'Whenever top inch of soil is dry', 2 ),
-    Plant('Prickly Pear', 'Opuntia', 'outdoor', 'direct light', '2-4 weeks (2/month in summer, 1/month rest of year', 1 ),
-    Plant('Prickly Pear', 'Opuntia', 'outdoor', 'direct light', '2-4 weeks (2/month in summer, 1/month rest of year', 1 ),
-    Plant('Prickly Pear', 'Opuntia', 'outdoor', 'direct light', '2-4 weeks (2/month in summer, 1/month rest of year', 1 ),
-    Plant('Prickly Pear', 'Opuntia', 'outdoor', 'direct light', '2-4 weeks (2/month in summer, 1/month rest of year', 1 ),
-    Plant('Prickly Pear', 'Opuntia', 'outdoor', 'direct light', '2-4 weeks (2/month in summer, 1/month rest of year', 1 ),
-    Plant('Prickly Pear', 'Opuntia', 'outdoor', 'direct light', '2-4 weeks (2/month in summer, 1/month rest of year', 1 ),
-    Plant('Prickly Pear', 'Opuntia', 'outdoor', 'direct light', '2-4 weeks (2/month in summer, 1/month rest of year', 1 ),
-    Plant('Prickly Pear', 'Opuntia', 'outdoor', 'direct light', '2-4 weeks (2/month in summer, 1/month rest of year', 1 )
-]
-
 def home(request):
-    return HttpResponse('<h1>Hello Plant Collector!</h1>')
+    return render(request, 'home.html')
 
 def about(request): 
     return render(request, 'about.html')
 
 def plants_index(request):
-    return render(request, 'plants/index.html', {
-        'plants': plants
-    })
+    plants = Plant.objects.all()
+    return render(request, 'plants/index.html', {'plants': plants})
+
+def plants_detail(request, plant_id):
+    plant = Plant.objects.get(id=plant_id)
+    return render(request, 'plants/detail.html', { 'plant': plant})
