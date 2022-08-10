f8000cba96bb84ba1bbcbbe4bb582603a68439e2
lilliesheely <lilliesheely@gmail.com>
2022-08-09 17:47:02 -0700

    1:M Model, form, CSS

--- main_app/models.py
+++ main_app/models.py
@@ -3,16 +3,29 @@ from django.urls import reverse
 
 # Create your models here.
 class Plant(models.Model): 
-    laymen_name = models.CharField(max_length=100)
+    common_name = models.CharField(max_length=100)
     latin_name = models.CharField(max_length=100)
     location = models.CharField(max_length=20)
     light = models.CharField(max_length=50)
     water = models.CharField(max_length=200)
     maintenance_level = models.IntegerField()
 
+
+
     def __str__(self):
-        return f'{self.laymen_name} ({self.id})' 
+        return f'{self.common_name} ({self.id})' 
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'plant_id': self.id})
- 
+     
+class Watering(models.Model):
+    date = models.DateField('Watering Date')
+
+    plant = models.ForeignKey(
+        Plant, 
+        on_delete=models.CASCADE
+    )
+
+    def __str__ (self):
+        return f"Watered on {self.date}"
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Plant
+from .forms import WateringForm
+# from datetime import datetime
 # Create your views here.
 
 
@@ -16,7 +18,12 @@ def plants_index(request):
 
 def plants_detail(request, plant_id):
     plant = Plant.objects.get(id=plant_id)
-    return render(request, 'plants/detail.html', { 'plant': plant})
+    watering_form = WateringForm()
+    return render(
+        request, 
+        'plants/detail.html', 
+        { 'plant': plant, 'watering_form': watering_form}
+    )
 
 class PlantCreate(CreateView):
     model = Plant
@@ -29,3 +36,16 @@ class PlantUpdate(UpdateView):
 class PlantDelete(DeleteView): 
     model = Plant
     success_url = '/plants/'    
+
+def add_watering(request, plant_id):
+    form = WateringForm(request.POST)
+    if form.is_valid():
+        new_watering = form.save(commit=False)
+        new_watering.plant_id = plant_id
+        new_watering.save()
+    return redirect('detail', plant_id=plant_id)
+
+# def watered_this_week(request):
+#   today = datetime.now()
+#   num_watering_this_week = Watering.objects.filter(date__year=today.year, date__month=today.month, date__week=today.week).count()
+  # respond with a render?    
